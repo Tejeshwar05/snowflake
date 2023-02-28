63d35c14423dfea331cd68e8efdd42aa8b58e11a
Tejeshwar05 <teja.ega@gmail.com>
2023-02-28 12:41:59 +0530

    modified

--- dbscripts/dbscripts.sql
+++ dbscripts/dbscripts.sql
@@ -1,6 +1,1 @@
-create or replace warehouse snow_cicd with warehouse_size='X-Large';
-use warehouse snow_cicd;
-create or replace database snow_cicd;
-use database snow_cicd
-create schema snow_cicd;
-create table first(name varchar(50));
+create table snow(name varchar(50));
